163f3c4038f29a1f3151c056cf4313ca144fe6e6
seemenkina <seemenkina@gmail.com>
2024-12-05 14:58:59 +0700

    add day5

--- day5/day5.nim
+++ day5/day5.nim
@@ -4,43 +4,6 @@ import std/strutils
 import std/sequtils
 import std/algorithm
 
-proc check_line(table: Table[int, HashSet[int]], seq_line: seq[int]): bool =
-  var if_ok = true
-  for i in 0..seq_line.len-1:
-    let seq_after_i = seq_line[i+1..^1]
-    let seq_before_i = seq_line[0..i-1]
-    let seq_after_i_set = seq_after_i.toHashSet()
-    let seq_before_i_set = seq_before_i.toHashSet()
-
-    let seq_for_table = table.getOrDefault(seq_line[i])
-    if seq_for_table == initHashSet[int]() and seq_after_i_set.len == 0:
-        if_ok = true
-    else: 
-        let intersection_before = seq_before_i_set * seq_for_table
-        if (seq_after_i_set <= seq_for_table) and (intersection_before.len == 0):
-            if_ok = true
-        else:
-            if_ok = false
-            break
-  return if_ok
-
-
-proc fix_line(table: Table[int, HashSet[int]], seq_line: seq[int]): seq[int] =
-  var seq_line_copy = seq_line
-  for i in 0..seq_line.len-1:
-    let current_num = seq_line_copy[i]
-    var i_copy = i
-    let seq_for_table = table.getOrDefault(current_num)
-    for j in i+1..seq_line.len-1:
-      let next_num = seq_line_copy[j]
-      if seq_for_table == initHashSet[int]() or next_num notin seq_for_table:
-        seq_line_copy[i_copy] = next_num
-        seq_line_copy[j] = current_num
-        i_copy = j
-      else:
-        continue
-  return seq_line_copy
-
 proc day5_a(): int =
   let file = open("day5_input.txt")
   defer: file.close()
@@ -58,13 +21,21 @@ proc day5_a(): int =
   for line in file.lines:
     let split_line = line.split(",")
     let int_line = split_line.mapIt(parseInt(it))
-    if check_line(table, int_line):
-      let middle_index: int = (int_line.len - 1) div 2
+    if int_line.isSorted(proc (a: int, b: int): int = 
+      let seq_for_table = table.getOrDefault(a)
+      if seq_for_table == initHashSet[int]() or b notin seq_for_table:
+        return -1
+      elif b in seq_for_table:
+        return 0
+      else:
+        return 1
+    , Descending):
+      let middle_index = (int_line.len - 1) div 2
       result += int_line[middle_index]
   return result
 
 proc day5_b(): int =
-  let file = open("day5_simple.txt")
+  let file = open("day5_input.txt")
   defer: file.close()
   var table: Table[int, HashSet[int]] = initTable[int, HashSet[int]]()
   for line in file.lines:
@@ -81,24 +52,20 @@ proc day5_b(): int =
   for line in file.lines:
     let split_line = line.split(",")
     var int_line = split_line.mapIt(parseInt(it))
-    
-    int_line.sort(proc (a: int, b: int): int = 
+    let sorted_int_line = int_line.sorted(proc (a: int, b: int): int = 
       let seq_for_table = table.getOrDefault(a)
       if seq_for_table == initHashSet[int]() or b notin seq_for_table:
         return -1
+      elif b in seq_for_table:
+        return 0
       else:
         return 1
-    )
+    , Descending)
 
-    echo check_line(table, int_line)
-    
-    # if not check_line(table, seq_line):
-    #   var seq_line_copy = fix_line(table, int_line)
-    #   while not check_line(table, seq_line_copy):
-    #     seq_line_copy = fix_line(table, seq_line_copy)
-    #   let middle_index = (seq_line_copy.len - 1) div 2
-    #   result += seq_line_copy[middle_index]
+    if sorted_int_line != int_line:
+      let middle_index = (sorted_int_line.len - 1) div 2
+      result += sorted_int_line[middle_index]
   return result
 
-# echo day5_a()
+echo day5_a()
 echo day5_b()
