ace243e5f2603e2965e55f4143e5c50c62b5404d
seemenkina <seemenkina@gmail.com>
2024-12-07 12:45:36 +0700

    add day07

--- day07/day07.nim
+++ day07/day07.nim
@@ -1,19 +1,87 @@
 import sequtils
-import sets
-import tables
+import strutils
+import algorithm
 
-proc readInput(): int =
-  let file = open("day07_simple.txt")
-  defer: file.close()
-  for line in file.lines():
-    result += 1
+proc calculate_day7_a(numbers: seq[int], operations: seq[char], sum: int): int =
+  result = numbers[0]
+  for i in 0..operations.len - 1:
+    if operations[i] == '+':
+      result += numbers[i + 1]
+    else:
+      result *= numbers[i + 1]
+    if result > sum:
+      return 0
   return result
 
-proc day6_a(): int = 
-  return readInput()
+proc permutations_day7_a(numbers: seq[int], sum: int): int =
+  result = 0
+  let count = numbers.len - 1
+  for i in 0..count:
+    var chars = "*".repeat(count - i) & "+".repeat(i)
+    if chars.len == 1:
+      if calculate_day7_a(numbers, chars.toSeq(), sum) == sum:
+        return sum
+    else:
+      chars.sort()
+      if calculate_day7_a(numbers, chars.toSeq(), sum) == sum:
+        return sum
+      while chars.nextPermutation():
+        if calculate_day7_a(numbers, chars.toSeq(), sum) == sum:
+          return sum
+  return 0
 
-proc day6_b(): int =
-  return readInput()
+proc calculate_day7_b(numbers: seq[int], operations: seq[char], sum: int): int =
+  result = numbers[0]
+  for i in 0..operations.len - 1:
+    if operations[i] == '+':
+      result += numbers[i + 1]
+    elif operations[i] == '*':
+      result *= numbers[i + 1]
+    else:
+      let res_str = result.intToStr()
+      let num_str = numbers[i + 1].intToStr()
+      result = parseInt(res_str & num_str)
+    if result > sum:
+      return 0
+  return result
 
-echo day6_a() 
-echo day6_b()
+proc permutations_day7_b(numbers: seq[int], sum: int): int =
+  result = 0
+  let count = numbers.len - 1
+  for i in 0..count:
+    for j in 0..count-i:
+      var chars = "*".repeat(count - j - i) & "+".repeat(i) & "|".repeat(j)
+      if chars.len == 1:
+        if calculate_day7_b(numbers, chars.toSeq(), sum) == sum:
+          return sum
+      else:
+        chars.sort()
+        if calculate_day7_b(numbers, chars.toSeq(), sum) == sum:
+          return sum
+        while chars.nextPermutation():
+          if calculate_day7_b(numbers, chars.toSeq(), sum) == sum:
+            return sum
+  return 0
+
+proc day7_a(): int = 
+  let file = open("day07_input.txt")
+  defer: file.close() 
+  for line in file.lines():
+    let parts = line.split(": ")
+    let res = parseInt(parts[0])
+    let numbers = parts[1].splitWhitespace().mapIt(parseInt(it))
+    result += permutations_day7_a(numbers, res)
+  return result
+
+proc day7_b(): int =
+  let file = open("day07_input.txt")
+  defer: file.close() 
+  for line in file.lines():
+    let parts = line.split(": ")
+    let res = parseInt(parts[0])
+    let numbers = parts[1].splitWhitespace().mapIt(parseInt(it))
+    result += permutations_day7_b(numbers, res)
+  return result
+
+echo day7_a() 
+echo day7_b()
